6f9723fc411e06428683a2544b880bcdb975be2c
realkhalife <realjacquesmakabi@gmail.com>
2022-04-25 09:45:33 +0200

    Test, comment

--- lib/data_logic/models/course_category.dart
+++ lib/data_logic/models/course_category.dart
@@ -4,13 +4,13 @@ class CourseCategory {
     this.imagePath = '',
     this.lessonCredit = 0,
     this.hours = 0,
-    //this.rating = 0.0,
+    this.rating = 0.0,
   });
 
   String title;
   int lessonCredit;
   int hours;
-  //double rating;
+  double rating;
   String imagePath;
 
 
