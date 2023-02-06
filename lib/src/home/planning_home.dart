9c53d849763aa2dc855ae17c2378f1dddd3961d4
lordyhas <dev.haspro@gmail.com>
2023-02-06 09:38:46 +0100

    created and still empty | version 0.0.4+5

---
+++ lib/src/home/planning_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class PlanningScreen extends StatelessWidget {
+  const PlanningScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
